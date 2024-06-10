78599a5fffec1e6b8e739a36653700455e867f35
Rocken <r0ck3n@hotmail.com>
2024-06-10 11:14:23 +0200

    exo fini

---
+++ src/Form/IngredientType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Cocktail;
+use App\Entity\Ingredient;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class IngredientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            // ->add('cocktails', EntityType::class, [
+            //     'class' => Cocktail::class,
+            //     'choice_label' => 'id',
+            //     'multiple' => true,
+            // ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Ingredient::class,
+        ]);
+    }
+}
